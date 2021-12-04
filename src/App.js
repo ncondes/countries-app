f7fc784fc111605edfbb244c387660d9269d23ca
Nicolas <ncondes@unal.edu.co>
2021-12-03 20:38:35 -0500

    starter webpack config

---
+++ src/App.js
@@ -0,0 +1,9 @@
+
+
+export const App = () => {
+    return (
+        <div>
+            <h1>APP</h1>
+        </div>
+    )
+}
---
+++ webpack.config.dev.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'), 
+    },
+    mode: 'development',
+    resolve: {
+        extensions: [ '.js', '.jsx' ],
+        alias:  {},
+    },
+    module : {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader',
+                },
+            },
+            {
+                test: /\.(css|s[ac]ss)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
+}
